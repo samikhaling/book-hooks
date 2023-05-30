a451d913a41c1fe99adac44fa5c879fcd4ad92db
Samir Rai <samikhaling14@gmail.com>
2023-05-30 08:07:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useEffect,useContext } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BookContext from "./context/book";
+function App() {    
+    const {fetchBooks} = useContext(BookContext);
+    useEffect(()=>{
+        fetchBooks();
+    },[]);
+    
+    return (
+        <div className="app">
+            <h1>Reading List</h1>
+            <BookList />
+            <BookCreate />
+        </div>
+    );
+    
 }
 
 export default App;
